a6def7df7679a2a425cabd086da35208237d0be3
RoroDev <rodrigotrejo098@gmail.com>
2024-09-10 08:50:14 -0500

    feat(migration): add migration and model for Category

---
+++ src/models/category.ts
@@ -0,0 +1,42 @@
+import { Optional } from 'sequelize';
+import { Model, Column, Table, DataType, HasMany } from 'sequelize-typescript';
+
+interface CategoryAttributes {
+    id: number;
+    name: string;
+    status: boolean;
+}
+
+interface CategoryCreationAttributes extends Optional<CategoryAttributes, 'id'> { }
+
+@Table({
+    modelName: 'Category',
+    timestamps: true,
+    tableName: 'categories',
+})
+class Category extends Model<CategoryAttributes, CategoryCreationAttributes> {
+    @Column({
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataType.INTEGER,
+    })
+    public id!: number;
+
+    @Column({
+        allowNull: false,
+        type: DataType.STRING,
+        unique: true,
+    })
+    public name!: string;
+
+    @Column({
+        allowNull: false,
+        type: DataType.BOOLEAN,
+        defaultValue: true,
+    })
+    public status!: boolean;
+
+}
+
+export default Category;
